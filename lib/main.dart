50e328277e5ee624a2767b7813ca324733568459
avashsth9 <avashsth@gmail.com>
2022-06-22 08:28:06 +0545

    created coluln

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hii'),
+      body: Column(
+        children: [
+          Text('Hii'),
+          Text('Hii'),
+        ],
+      ),
     );
   }
 }
